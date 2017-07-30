3f9ba9e4d5e41f216984c53b588306b8571f7475
KaluginDV <interrupted-man@yandex.ru>
2017-07-30 15:00:39 +0800

    Add to project my first prog - geekbrains

---
+++ 01_FirstProg/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_FirstProg
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Я хочу изучать C#!");
+			Console.ReadLine();
+		}
+	}
+}
